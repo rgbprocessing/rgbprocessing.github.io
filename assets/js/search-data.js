9625f820e239ff964c9f878a94b0b00191c6c9e7
rgbprocessing <168565443+rgbprocessing@users.noreply.github.com>
2026-01-07 18:49:46 +0000

    Deploying to gh-pages from @ rgbprocessing/rgbprocessing.github.io@aacb44eacd9bd9ecf4d484581c5037949675ae33 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
+        },{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {

c9eed67f38474fbfa6149b49e48ce3de7f6cb579
rgbprocessing <168565443+rgbprocessing@users.noreply.github.com>
2026-04-03 21:27:47 +0000

    Deploying to gh-pages from @ rgbprocessing/rgbprocessing.github.io@705f8f57c8d7464ec995435393199d0cdafe0779 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "a selection of past work on competitions, research, and tools",
+          description: "a selection of past work on research, competitions, and tools",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
